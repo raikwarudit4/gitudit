e99fea1c6830155b38f87fcab6eaa953ef4ccaef
Udit narayan <38422197+raikwarudit4@users.noreply.github.com>
2018-04-16 14:47:51 +0530

    Add files via upload

---
+++ argv0.c
@@ -0,0 +1,15 @@
+// Printing a command-line argument
+
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc == 2)
+    {
+        printf("hello, %s\n", argv[1]);
+    }
+    else
+    {
+        printf("hello, world\n");
+    }
+}
